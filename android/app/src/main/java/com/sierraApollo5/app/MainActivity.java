40c65fa90607b6f32e6248ba9f11e707644209b6
chini <chinmayasharma@proton.me>
2025-05-04 02:53:53 +0530

    react and capacitor project initialized with vite

---
+++ android/app/src/main/java/com/sierraApollo5/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sierraApollo5.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sierraApollo5.app',
+  appName: 'sierra-apollo-5',
+  webDir: 'dist'
+};
+
+export default config;

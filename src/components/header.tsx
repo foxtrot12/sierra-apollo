cd54b05525dcad7043cf9a65fb06937854084a3b
chini <chinmayasharma@proton.me>
2025-05-06 00:47:56 +0530

    shopping cart icon added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
-import { MenuIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { memo } from "react";
 
 function Header() {
   return (
-    <nav className="flex w-full h-12 justify-between px-1.5 border-b-2 border-accent">
+    <nav
+      className="flex w-full h-12 justify-between px-2 
+    border-b-2 border-accent"
+    >
       <img alt="Sierra Apollo" src="/logoLandscape.png"></img>
       <button className="bg-none border-none cursor-pointer">
-        <MenuIcon className="text-accent h-full" />
+        <ShoppingCartIcon className="text-secondary h-full" />
       </button>
     </nav>
   );

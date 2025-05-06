f93f79b740380ae329126cf9c7b12a846935467b
chini <chinmayasharma@proton.me>
2025-05-06 11:25:22 +0530

    product card updated

--- src/components/productCard/priceTag.tsx
+++ src/components/productCard/priceTag.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import { useTranslation } from "react-i18next";
 
 export interface PriceTagParams {
   price: number;
@@ -7,10 +8,13 @@ export interface PriceTagParams {
 }
 
 function PriceTag(props: PriceTagParams) {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col h-full w-full">
-      <div className="flex bg-accent py-4 font-bold text-secondary 
-      justify-center rounded-t-lg">{`${props.discountPercentage}% off`}</div>
+      <div
+        className="flex bg-accent py-4 font-bold text-secondary 
+      justify-center rounded-t-lg"
+      >{`${props.discountPercentage}% ${t("off")}`}</div>
       <div className="flex bg-primary flex-col p-2.5 items-center rounded-b-2xl">
         <div className="text-secondary justify-center opacity-85 text-lg line-through">
           ₹{props.originalPrice}
--- src/components/productCard/productCard.tsx
+++ src/components/productCard/productCard.tsx
@@ -1,37 +1,38 @@
 import { memo } from "react";
 import TextCase from "../textCase";
 import PriceTag, { PriceTagParams } from "./priceTag";
+import { useTranslation } from "react-i18next";
 
 export interface ProductCardParams extends PriceTagParams {
-  id: string;
+  id: string | number;
   name: string;
   imageUrl: string;
   brand: string;
-  swipeDirection?: "left" | "right" | "up";
 }
 
 function ProductCard(props: ProductCardParams) {
+  const { t } = useTranslation();
   return (
-    <div className="flex w-full h-5/6 flex-col relative pt-1">
+    <div className="flex w-full h-11/12 flex-col relative pt-1">
       <img
         src={props.imageUrl}
         className="h-full border-transparent border-2 rounded-2xl"
       />
-      <div className="flex justify-between">
-        <div></div>
-        <div></div>
-      </div>
       <div>
         <div
           className="flex flex-col w-full absolute bottom-0 p-2 font-bold 
         bg-gradient-to-t from-primary to-transparent"
         >
+          <div className="flex justify-between"></div>
           <div className="text-4xl text-secondary">
             <TextCase mode={"capitalize"}>{props.name}</TextCase>
           </div>
+
           <div className="text-2xl items-end text-secondary flex flex-col">
             <div className="flex flex-col">
-              <span className="text-accent -translate-x-5">By</span>
+              <span className="text-accent -translate-x-5">
+                <TextCase mode={"capitalize"}>{t("by")}</TextCase>
+              </span>
               <TextCase mode={"capitalize"}>{props.brand}</TextCase>
             </div>
           </div>

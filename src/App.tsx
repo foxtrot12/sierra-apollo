d6e41730f6b524132365d96dfb4407ecb754e52c
chini <chinmayasharma@proton.me>
2025-05-09 06:33:22 +0530

    route list added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import ProductCardCarousel from "./components/productCardCarousel";
-import { Test } from "./components/test";
 import { ProductProvider } from "./contexts/productStateContext";
 import { Products } from "./mockProducts";
+import Cart from "./components/cart/cart";
+import { RoutesList } from "./routes";
 
 function App() {
   return (
     <ProductProvider>
-      <Test/>
       <main className="w-screen h-screen flex font-mono items-center flex-col bg-primary">
-        <Header />
-        <ProductCardCarousel products={Products} />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route
+              path={RoutesList.home}
+              element={<ProductCardCarousel products={Products} />}
+            />
+            <Route path={RoutesList.cart} element={<Cart/>} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </ProductProvider>
   );

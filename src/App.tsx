0c1436cd9f62bd95090e5da7f54eb0ba51e669fc
chini <chinmayasharma@proton.me>
2025-05-09 20:12:17 +0530

    native back support added for android

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import ProductCardCarousel from "./components/productCardCarousel";
 import { ProductProvider } from "./contexts/productStateContext";
 import { Products } from "./mockProducts";
 import Cart from "./components/cart/cart";
 import { RoutesList } from "./routes";
+import { App as CapApp } from "@capacitor/app";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    CapApp.addListener("backButton", () => {
+      if (location.pathname === RoutesList.home) {
+        CapApp.exitApp();
+      } else {
+        navigate(-1);
+      }
+    });
+
+    return () => {
+      CapApp.removeAllListeners();
+    };
+  }, [location, navigate]);
 
   return (
     <ProductProvider>
       <main className="w-full h-full flex font-mono items-center flex-col bg-primary">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route
-              path={RoutesList.home}
-              element={<ProductCardCarousel products={Products} />}
-            />
-            <Route path={RoutesList.cart} element={<Cart/>} />
-          </Routes>
-        </BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path={RoutesList.home}
+            element={<ProductCardCarousel products={Products} />}
+          />
+          <Route path={RoutesList.cart} element={<Cart />} />
+        </Routes>
       </main>
     </ProductProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./i18n";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<div>Loading…</div>}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Suspense>
   </StrictMode>
 );
